b35d27b6747c7d168081a5c973fc78c1ba18ae4d
StephenHolzman <stephen@holzmansmith.com>
2016-05-18 06:34:08 -0400

    Update Multiline, Set default background color to white

--- R/basicBoxplot.R
+++ R/basicBoxplot.R
@@ -1,5 +1,5 @@
 #Plotting
-basicBoxplot <- function(data, xvar, yvar, colourvar = NULL, stat = "boxplot", pos = "dodge", flip = TRUE, title = "", subtitle = "", legendtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#B2D9A2","#A0DADD", "#EFC786", "#919191", "#E2B9D7","#A05F6B","000000"), width = 800, height = 500, plotbackground = '#E8E8E8',  headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white", fontfamily = "Arial") {
+basicBoxplot <- function(data, xvar, yvar, colourvar = NULL, stat = "boxplot", pos = "dodge", flip = TRUE, title = "", subtitle = "", legendtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#B2D9A2","#A0DADD", "#EFC786", "#919191", "#E2B9D7","#A05F6B","000000"), width = 800, height = 500, plotbackground = 'white',  headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white", fontfamily = "Arial") {
   
   if(is.character(colourvar)){
     p <- ggplot(data, aes_string(x = xvar, y = yvar, fill = colourvar))
--- R/basicScatter.R
+++ R/basicScatter.R
@@ -1,5 +1,5 @@
 #Plotting
-basicScatter <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos = "dodge", flip = TRUE, title = "", subtitle = "", legendtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#B2D9A2","#A0DADD", "#EFC786", "#919191", "#E2B9D7","#A05F6B","000000"), width = 800, height = 500, plotbackground = '#E8E8E8',  headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white", fontfamily = "Arial") {
+basicScatter <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos = "dodge", flip = TRUE, title = "", subtitle = "", legendtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#B2D9A2","#A0DADD", "#EFC786", "#919191", "#E2B9D7","#A05F6B","000000"), width = 800, height = 500, pointsize = 4, plotbackground = '#E8E8E8',  headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white", fontfamily = "Arial") {
 
   if(is.character(colourvar)){
     p <- ggplot(data, aes_string(x = xvar, y = yvar, colour = colourvar, fill = NULL))
@@ -24,14 +24,13 @@ basicScatter <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", 
                  axis.title.x = element_text(size = rel(1.8),margin=margin(20,0,0,0),family=fontfamily,face="bold"),
                  panel.background = element_rect(fill = plotbackground, colour = plotbackground),
                  legend.key.size = unit(.05,"npc"),
-                legend.key = element_rect(fill = plotbackground, colour = plotbackground),
+                 legend.key = element_rect(fill = plotbackground, colour = plotbackground),
                    legend.title = element_text(size = rel(1.8),margin=margin(0,20,0,50),family=fontfamily,face="bold"),
                    legend.position = 'right',
                    legend.text = element_text(face = "bold",size = 14),
                    #legend.margin = unit(1, "cm"),
                    legend.background = element_rect(fill = plotbackground, colour = plotbackground),
                    plot.background = element_rect(fill = plotbackground, colour = plotbackground)) 
-  p <- p + geom_point(size=4)
 
   
   if(class(data[[xvar]]) == "Date"){
@@ -64,6 +63,12 @@ basicScatter <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", 
   p <- p + ylab(ytitle)
   p <- p + labs(colour = legendtitle)
   p <- p + scale_colour_manual(values = colpal)
+  if(is.character(pointsize)){
+    p <- p + scale_size("")
+    p <- p + geom_point(aes_string(size = pointsize),alpha = .7)
+  }else{
+    p <- p + geom_point(size = pointsize)
+  }
 
   #Save to PNG
   savePNG(plot = p, path = path, width = width, height = height, title = title, subtitle = subtitle, cite = cite, author = author, headerbackground = headerbackground, headerfontcol = headerfontcol, footerbackground = footerbackground, footerfontcol = footerfontcol, fontfamily = fontfamily) 
