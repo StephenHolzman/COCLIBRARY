f6ee4d6036b8dbc1299ca5b41d9d5f525e4a0397
StephenHolzman <stephen@holzmansmith.com>
2016-05-18 02:58:18 -0400

    basicBar beta version done
    Default arguments provided and fails nicely. Support for custom colors,
    fonts, titles, etc. Need to really think about argument names, but for
    now they should be reasonably self explanatory.

--- R/basicBar.R
+++ R/basicBar.R
@@ -1,5 +1,5 @@
 #Plotting
-basicBar <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos = "dodge", flip = TRUE, title = "", subtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#A0DADD", "#EFC786", "#919191", "#E2B9D7", "#B2D9A2","#A05F6B","000000"), width = 800, height = 500) {
+basicBar <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos = "dodge", flip = TRUE, title = "", subtitle = "", legendtitle = "", cite = "", author = "", path = NULL,  xtitle = "", xlimits = NULL, xbreaks = NULL, xlabels = NULL, ytitle = NULL, ylimits = NULL, ybreaks = NULL, ylabels = NULL, colpal = c("#B3B0D8", "#E4E499", "#F8ADAD", "#B2D9A2","#A0DADD", "#EFC786", "#919191", "#E2B9D7","#A05F6B","000000"), width = 800, height = 500, plotbackground = '#E8E8E8',  headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white") {
 
   if(is.character(colourvar)){
     p <- ggplot(data, aes_string(x = xvar, y = yvar, fill = colourvar))
@@ -15,7 +15,7 @@ basicBar <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos 
                    panel.grid.minor.x = element_blank(),
                    panel.grid.minor.y = element_blank(),
                    panel.grid.major.y = element_blank(),
-                   plot.margin = unit(c(7, 2.5, 4, 2), "lines"),
+                   plot.margin = unit(c(7, 0, 4, 1), "lines"),
                    axis.text = element_text(face = "bold", size = rel(1.3)),
                    axis.ticks = element_line(colour = NULL),
                    axis.ticks.y = element_blank(),
@@ -24,17 +24,21 @@ basicBar <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos 
                    #axis.line.y = element_blank(),
                    axis.title.y = element_text(size = rel(1.8), angle = 90,margin=margin(0,20,0,0),family="Arial",face="bold"),
                    axis.title.x = element_text(size = rel(1.8),margin=margin(20,0,0,0),family="Arial",face="bold"),
-                   panel.background = element_rect(fill = 'white'),
+                   panel.background = element_rect(fill = plotbackground, colour = plotbackground),
                    legend.key.size = unit(.05,"npc"),
-                   legend.key = element_rect(fill = 'white'),
-                   legend.title = element_blank(),
-                   legend.text = element_text(face = "bold",size = 14))    
+                   legend.key = element_rect(fill = plotbackground, colour = plotbackground),
+                   legend.title = element_text(size = rel(1.8),margin=margin(0,20,0,50),family="Arial",face="bold"),
+                   legend.position = 'right',
+                   legend.text = element_text(face = "bold",size = 14),
+                   #legend.margin = unit(1, "cm"),
+                   legend.background = element_rect(fill = plotbackground, colour = plotbackground),
+                   plot.background = element_rect(fill = plotbackground, colour = plotbackground))    
   }else{
     p <- p + theme(panel.grid.major.x = element_blank(),
                    panel.grid.minor.x = element_blank(),
                    panel.grid.minor.y = element_blank(),
                    panel.grid.major.y = element_line(colour = "#AAAAAA"),
-                   plot.margin = unit(c(7, 2.5, 4, 2), "lines"),
+                   plot.margin = unit(c(7, 2, 4, 2), "lines"),
                    axis.text = element_text(face = "bold", size = rel(1.3)),
                    axis.ticks = element_line(colour = NULL),
                    axis.ticks.y = element_blank(),
@@ -43,29 +47,36 @@ basicBar <- function(data, xvar, yvar, colourvar = NULL, stat = "identity", pos 
                    #axis.line.y = element_blank(),
                    axis.title.y = element_text(size = rel(1.8), angle = 90,margin=margin(0,20,0,0),family="Arial",face="bold"),
                    axis.title.x = element_text(size = rel(1.8),margin=margin(20,0,0,0),family="Arial",face="bold"),
-                   panel.background = element_rect(fill = 'white'),
+                   panel.background = element_rect(fill = plotbackground, colour = plotbackground),
                    legend.key.size = unit(.05,"npc"),
-                   legend.key = element_rect(fill = 'white'),
-                   legend.title = element_blank(),
-                   legend.text = element_text(face = "bold",size = 14))    
+                   legend.key = element_rect(fill = plotbackground, colour = plotbackground),
+                   legend.title = element_text(size = rel(1.8),margin=margin(0,20,0,50),family="Arial",face="bold"),
+                   legend.position = 'right',
+                   #legend.margin = unit(c(7, 2.5, 4, 2), "lines"),
+                   legend.text = element_text(face = "bold",size = 14),
+                   legend.background = element_rect(fill = plotbackground,colour = plotbackground),
+                   plot.background = element_rect(fill = plotbackground, colour = plotbackground))    
   }
 
   
-  
-  p <- p + scale_y_continuous(limits = ylimits,breaks = ybreaks, labels = ylabels, expand = c(0,0))
+  if(is.vector(ybreaks)){
+    if(is.vector(ylabels)){
+      p <- p + scale_y_continuous(limits = ylimits,breaks = ybreaks, labels = ylabels, expand = c(0,0))
+    }else{
+      p <- p + scale_y_continuous(limits = ylimits,breaks = ybreaks, expand = c(0,0))
+    }
+  }
+  if(is.vector(xlabels)){
+    p <- p + scale_x_discrete(labels = xlabels)
+  }
   p <- p + xlab(xtitle)
   p <- p + ylab(ytitle)
+  p <- p + labs(fill = legendtitle)
   if(flip){
     p <- p + coord_flip()   
   }
+  
   #Save to PNG
-  png(path, width = width, height = height)
-  print(p)
-  grid.rect(x=unit(.3,"npc"),y=unit(0,"npc"),width=unit(2,"npc"),height=unit(75/height,"npc"),gp=gpar(fill="#2E2E2E"))
-  grid.rect(x=unit(.3,"npc"),y=unit(1,"npc"),width=unit(2,"npc"),height=unit(150/height,"npc"),gp=gpar(fill="#2E2E2E"))
-  grid.text(title, x=unit(0.01,"npc"),y=unit((height-26)/height,"npc"),just="left",gp=gpar(fontsize=36,fontfamily="Arial",fontface="bold", col="white"))
-  grid.text(subtitle, x=unit(0.01,"npc"),y=unit((height-56)/height,"npc"),just="left",gp=gpar(fontsize=18,fontfamily="Arial",fontface="bold", col="white"))
-  grid.text(cite, x=unit(0.01,"npc"),y=unit(20/height,"npc"),just="left",gp=gpar(fontsize=18,fontfamily="Arial",fontface="bold", col="white"))
-  grid.text(author, x=unit(.99,"npc"),y=unit(20/height,"npc"),just="right",gp=gpar(fontsize=18,fontfamily="Arial",fontface="bold", col="white"))
-  dev.off()  
+  savePNG(plot = p, path = path, width = width, height = height, title = title, subtitle = subtitle, cite = cite, author = author, headerbackground = headerbackground, headerfontcol = headerfontcol, footerbackground = footerbackground, footerfontcol = footerfontcol)
+
 }
--- R/savePNG.R
+++ R/savePNG.R
@@ -1,8 +1,8 @@
-savePNG <- function(plot = NULL, path = getwd(), width = 600, height = 600, title = "", subtitle = "", cite = "", author = "", headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "#2E2E2E"){
+savePNG <- function(plot = NULL, path = getwd(), width = 600, height = 600, title = "", subtitle = "", cite = "", author = "", headerbackground = "#2E2E2E", headerfontcol = "white", footerbackground = "#2E2E2E", footerfontcol = "white"){
   png(path, width = width, height = height)
   print(plot)
-  grid.rect(x=unit(.3,"npc"),y=unit(0,"npc"),width=unit(2,"npc"),height=unit(75/height,"npc"),gp=gpar(fill=footerbackground))
-  grid.rect(x=unit(.3,"npc"),y=unit(1,"npc"),width=unit(2,"npc"),height=unit(150/height,"npc"),gp=gpar(fill=headerbackground))
+  grid.rect(x=unit(.3,"npc"),y=unit(0,"npc"),width=unit(2,"npc"),height=unit(75/height,"npc"),gp=gpar(fill=footerbackground, col = footerbackground))
+  grid.rect(x=unit(.3,"npc"),y=unit(1,"npc"),width=unit(2,"npc"),height=unit(150/height,"npc"),gp=gpar(fill=headerbackground, col = headerbackground))
   grid.text(title, x=unit(0.01,"npc"),y=unit((height-26)/height,"npc"),just="left",gp=gpar(fontsize=36,fontfamily="Arial",fontface="bold", col=headerfontcol))
   grid.text(subtitle, x=unit(0.01,"npc"),y=unit((height-56)/height,"npc"),just="left",gp=gpar(fontsize=18,fontfamily="Arial",fontface="bold", col=headerfontcol))
   grid.text(cite, x=unit(0.01,"npc"),y=unit(20/height,"npc"),just="left",gp=gpar(fontsize=18,fontfamily="Arial",fontface="bold", col=footerfontcol))
